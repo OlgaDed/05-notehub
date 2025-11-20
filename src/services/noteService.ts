9e3cc3884fda52425249631bf254c26abc70971e
OlgaDed <garmoneya@ukr.net>
2025-11-20 02:24:48 +0200

    token

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,34 +1,7 @@
 import axios from 'axios';
-import type { Movie } from '../types/note';
 
-const API_BASE_URL = 'https://api.themoviedb.org/3';
-const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+const token = import.meta.env.VITE_NOTEHUB_TOKEN;
 
-interface TMDBResponse {
-  page: number;
-  results: Movie[];
-  total_pages: number;
-  total_results: number;
-}
+axios.defaults.baseURL = 'https://notehub-public.goit.study/api';
 
-export const fetchMovies = async (
-  query: string,
-  page: number = 1
-): Promise<TMDBResponse> => {
-  const response = await axios.get<TMDBResponse>(
-    `${API_BASE_URL}/search/movie`,
-    {
-      params: {
-        query,
-        page,
-        include_adult: false,
-        language: 'en-US',
-      },
-      headers: {
-        Authorization: `Bearer ${TMDB_TOKEN}`,
-      },
-    }
-  );
-
-  return response.data;
-};
+axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
